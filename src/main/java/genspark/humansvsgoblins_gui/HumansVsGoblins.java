8ba0695d2b093962465ad7781ef2a8e226e58ced
Michael Guirguis <guirguis@mguirguis.com>
2022-05-03 14:54:52 -0400

    renaming controller and main class

--- src/main/java/genspark/humansvsgoblins_gui/HumansVsGoblins.java
+++ src/main/java/genspark/humansvsgoblins_gui/HumansVsGoblins.java
@@ -7,17 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
-
+public class HumansVsGoblins extends Application {
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HumansVsGoblins.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Humans Vs. Goblins");
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/main/java/genspark/humansvsgoblins_gui/MainController.java
+++ src/main/java/genspark/humansvsgoblins_gui/MainController.java
@@ -3,7 +3,7 @@ package genspark.humansvsgoblins_gui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
