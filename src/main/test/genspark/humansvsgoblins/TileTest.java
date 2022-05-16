062e6fa3722c4c959f1d1b4506d63a0870cca846
Michael Guirguis <guirguis@mguirguis.com>
2022-05-15 23:42:52 -0400

    fixed JavaFX testing

---
+++ src/main/test/genspark/humansvsgoblins/TileTest.java
@@ -0,0 +1,44 @@
+package genspark.humansvsgoblins;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TileTest extends ApplicationTest {
+
+
+    @Override
+    public void start(Stage stage) {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), 650, 550);
+        } catch (Exception e) {
+            System.out.println("Could not open main-view.fxml\n" + e.getLocalizedMessage());
+            System.exit(1);
+        }
+        stage.setScene(scene);
+    }
+
+    Tile tile;
+
+    @BeforeEach
+    void setUp() {
+
+        tile = new Tile();
+    }
+
+    @Test
+    void setTile() {
+        Piece piece = new Piece();
+        piece.shape = "s";
+        tile.setTile(piece);
+        assertEquals("s", tile.label.getText());
+    }
+}
